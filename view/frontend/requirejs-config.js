0dfd02b11d5beb6c4f7aff34aeb3a0b3fcb36190
Francisco <francisco@payapi.io>
2017-03-02 16:38:54 +0100

    Update require-js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,19 +1,21 @@
 var config = {
-    paths: {
-        payapiSdk: 'Payapi_CheckoutPayment/js/payapi-sdk.min',
-        fngrtouch: 'Payapi_Catalog/fngrsharesdk/touch.min',
-        fngrui: 'Payapi_Catalog/fngrsharesdk/sdk-ui.min',
-        fngrsdk: 'Payapi_Catalog/fngrsharesdk/sdk-controller.min'
+    map: {
+        '*': {
+            "payapiSdk": 'Payapi_CheckoutPayment/js/payapi-sdk.min',
+            "fngrtouch": 'Payapi_Catalog/fngrsharesdk/touch.min',
+            "fngrui": 'Payapi_Catalog/fngrsharesdk/sdk-ui.min',
+            "fngrsdk": 'Payapi_Catalog/fngrsharesdk/sdk-controller.min'
+        }
     },
     shim: {
         'fngrtouch': {
             deps: ['jquery']
+        } ,
+        'fngrui':  {
+            deps: ['jquery','fngrtouch']
         },
-        'fngrui': {
-            deps: ['jquery', 'fngrtouch']
-        },
-        'fngrsdk': {
-            deps: ['jquery', 'fngtouch', 'fngrui']
+        'fngrsdk':  {
+            deps: ['jquery','fngtouch','fngrui']
         }
     }
 };
