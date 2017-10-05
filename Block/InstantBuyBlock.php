146dee25126a85756bf22b2f042848ab94c83380
Francisco <francisco@payapi.io>
2017-10-05 10:36:47 +0200

    PayApi generic branding

--- Block/InstantBuyBlock.php
+++ Block/InstantBuyBlock.php
@@ -7,74 +7,47 @@ class InstantBuyBlock extends \Magento\Framework\View\Element\Template
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Payment\Helper\Data $paymentHelper,
-        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress,
-        \Payapi\CheckoutPayment\Model\Config\AllActiveShippingMethods $allShippingMethods,
+        \Payapi\CheckoutPayment\Block\Adminhtml\PayapiPluginConfiguration $payapiConfig,        
         array $data = []
     ) {
-        $this->paymentHelper = $paymentHelper;
-        $this->remoteAddress = $remoteAddress;
-        $this->allShippingMethods = $allShippingMethods;
+        $this->payapiConfig = $payapiConfig;        
         parent::__construct($context, $data);
     }
 
     public function checkPayApiConfiguration()
     {
-        $paymentMethod                   = $this->paymentHelper->getMethodInstance("payapi_checkoutpayment_secure_form_post");
-        $this->payapiApiKey              = $paymentMethod->getConfigData('payapi_api_key');
-        $this->payapiPublicId            = $paymentMethod->getConfigData('payapi_public_id');
-        $this->instantBuyDefaultShipping = $paymentMethod->getConfigData('instantbuy_shipping_method');
-        $this->isInstantBuyEnabled       = $paymentMethod->getConfigData('instantbuy_enabled');
-        $this->isStaging                 = $paymentMethod->getConfigData('staging');
-        $this->isEnabled                 = $paymentMethod->getConfigData('active');
-
-        return $this->isEnabled && isset($this->payapiPublicId) && isset($this->payapiApiKey) && isset($this->instantBuyDefaultShipping) && is_string($this->instantBuyDefaultShipping) && strlen($this->instantBuyDefaultShipping) > 0 && $this->allShippingMethods->contains($this->instantBuyDefaultShipping);
+        return $this->payapiConfig->checkPayApiConfiguration();
     }
 
     public function getPublicId()
     {
-        return $this->payapiPublicId;
+        return $this->payapiConfig->getPublicId();
     }
 
     public function getApiKey()
     {
-        return $this->payapiApiKey;
+        return $this->payapiConfig->getApiKey();
     }
 
     public function getIsInstantBuyEnabled(){
-        return $this->isInstantBuyEnabled;
+        return $this->payapiConfig->getIsInstantBuyEnabled();
     }
 
     public function getIsStaging()
     {
-        return $this->isStaging;
+        return $this->payapiConfig->getIsStaging();
     }
 
     public function getVisitorIp($checkParams = true)
     {
-        $ipaddress = '';
-        $paramIp   = $this->getRequest()->getQueryValue('ip');
-        if ($checkParams && $paramIp) {
-            return $paramIp;
-        } else {
-            $ipaddress = $this->getRequest()->getClientIp();
-        }
-        return $ipaddress;
+        return $this->payapiConfig->getVisitorIp($checkParams);
     }
 
-    public function checkMandatoryFields()
-    {
-        if ($this->getProduct()) {
-            $customOptions = $this->getProduct()->getProductOptionsCollection();
-            if ($customOptions) {
-                foreach ($customOptions as $o) {
-                    if ($o->getIsRequire()) {
-                        // or another title of option
-                        return 1;
-                    }
-                }
-            }
-        }
-        return 0;
+    public function getPhpSdk(){
+        return $this->payapiConfig->getPhpSdk();
+    }
+
+    public function getPartner() {  
+        return $this->payapiConfig->getPartner();
     }
 }
